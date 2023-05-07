a154414b5dde88ab8a9812c67d20064216058d2e
Thales Morais <thalesmn2017@gmail.com>
2023-05-07 03:18:30 -0300

    Adição de elementos final, projeto concluído!

---
+++ alt/README.md
@@ -0,0 +1,2 @@
+# Cronômetro
+Um componente que apresentará o tempo corrido de um cronômetro. O componente deverá apresentar o tempo corrido no formato mm:ss.milisegs, e o botão iniciar/parar.
--- js/script.js
+++ js/script.js
@@ -1,41 +1,77 @@
-const minutesE1 = document.querySelector("minutes")
-const secondsE1 = document.querySelector("secons")
-const milisecondsE1 = document.querySelector("miliseconds")
-const startBtn = document.querySelector("startBtn")
-const pauseBtn = document.querySelector("pauseBtn")
-const resumeBtn = document.querySelector("resumeBtn")
-const resetBtn = document.querySelector("resetBtn")
+const minutesE1 = document.querySelector("#minutes")
+const secondsE1 = document.querySelector("#seconds")
+const milisecondsE1 = document.querySelector("#miliseconds")
+const startBtn = document.querySelector("#startBtn")
+const pauseBtn = document.querySelector("#pauseBtn")
+const resumeBtn = document.querySelector("#resumeBtn")
+const resetBtn = document.querySelector("#resetBtn")
 
 let interval;
 let minutes = 0;
 let seconds = 0;
 let miliseconds = 0;
-isPaused = false;
+let isPaused = false;
 
-startBtn.addEventListener("click", startTimer)
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
 
 function startTimer(){
-
     interval = setInterval(() => {
 
         if (!isPaused) {
             miliseconds += 10;
         
-        if (miliseconds === 1000) {
+         if (miliseconds === 1000) {
             seconds++;
             miliseconds = 0;
-        }
-        if (seconds === 60) {
+         }
+         if (seconds === 60) {
             minutes++;
             seconds = 0;
-        }
-        minutes.textContent = minutes;
-        secondsE1.textContent = seconds;
-        miliseconds.textContent = miliseconds;
+         }
+        minutesE1.textContent = formatTime(minutes);
+        secondsE1.textContent = formatTime(seconds);
+        milisecondsE1.textContent = formatMiliseconds(miliseconds);
         }
     }, 10)
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block";
 }
 
-function formatTime(time){
-    
+function pauseTimer(){
+    isPaused = true
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "block";
+}
+
+function resumeTimer(){
+    isPaused = false
+    pauseBtn.style.display = "block";
+    resumeBtn.style.display = "none";
+}
+
+function resetTimer(){
+    isPaused = false;
+    clearInterval(interval);
+    minutes = 0;
+    seconds = 0;
+    miliseconds = 0;
+
+    minutesE1.textContent = "00"
+    secondsE1.textContent = "00"
+    milisecondsE1.textContent = "000"
+
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "none";
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+function formatMiliseconds(time) {
+    return time < 100 ? `${time}`.padStart(3, "0") : time;
 }
